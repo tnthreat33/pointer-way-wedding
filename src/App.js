21bf5827a000a24d9dceafb04df503ae8c7b7c5c
Rachel Hamby <tnthreat33@gmail.com>
2024-03-11 20:09:42 -0400

    install tailwind

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import './App.css';
 import Navbar from './NavBar';
 
 function App() {
+  const imageUrl = 'https://i.pinimg.com/originals/0d/d5/58/0dd558b702946287f8e5e49686f81b0b.jpg';
+
   return (
-    <>
-    <Navbar/>
-    <h1>Hi</h1>
-    <h2>we good</h2>
-    </>
+    <div className="bg-cream-tan min-h-screen">
+      <Navbar />
+      <img src={imageUrl} alt="Main Wedding Image" className="w-full" />
+    </div>
   );
 }
 
