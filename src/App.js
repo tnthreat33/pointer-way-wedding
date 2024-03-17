25c5ee7e43ad96046a696736a3aa2d9f46d5b96f
Rachel Hamby <tnthreat33@gmail.com>
2024-03-17 13:49:48 -0400

    outline what the main page will be

--- src/App.js
+++ src/App.js
@@ -7,6 +7,11 @@ function App() {
     <div className="bg-cream-tan min-h-screen">
       <Navbar />
       <img src={imageUrl} alt="Main Wedding Image" className="w-full" />
+      <p1> brief paragraph about venue</p1>
+      <p1> insert photo gallery</p1>
+      <p1> insert calendar with available dates</p1>
+      <p1> this will be a form to submit request</p1>
+      <p1> bottom bar with contact details and social links</p1>
     </div>
   );
 }
