eca1ff6ac498a436849c1dcabe0ad8bd3db959e7
Rachel Hamby <tnthreat33@gmail.com>
2024-03-26 19:26:19 -0400

    paragraph updated

--- src/App.js
+++ src/App.js
@@ -7,14 +7,14 @@ function App() {
     <div className="bg-cream-tan min-h-screen">
       <Navbar />
       <img src={imageUrl} alt="Main Wedding Image" className="w-full" />
-      <p1> brief paragraph about venue</p1>
+      <p1> Celebrate your special day amidst the picturesque charm of East Tennessee at Pointer's Way. Nestled in the heart of this stunning landscape, our family-owned venue offers a beautiful and affordable setting for your wedding festivities. From intimate ceremonies to grand celebrations, we provide various options tailored to your vision. Let us guide you every step of the way, ensuring that your dream wedding becomes a reality without straining your budget. Choose Pointer's Way and embark on your journey into married life with ease and elegance.</p1>
 
       <p1> insert photo gallery</p1>
 
       <p1> insert calendar with available dates</p1>
 
       <p1> this will be a form to submit request</p1>
-      
+
       <p1> bottom bar with contact details and social links</p1>
     </div>
   );
