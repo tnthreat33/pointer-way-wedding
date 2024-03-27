2807bcb2d84378b102a77e07ba2f0f7fe1ba1f61
Rachel Hamby <tnthreat33@gmail.com>
2024-03-27 11:16:23 -0400

    removed Calendar vanilla css added tailwind css

--- src/Calendar.js
+++ src/Calendar.js
@@ -2,22 +2,22 @@ import React, { useState } from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 
-
 const MyCalendar = ({ availableDates }) => {
   const [date, setDate] = useState(new Date());
 
   const tileClassName = ({ date }) => {
     // Check if the date is in the availableDates array
     const formattedDate = date.toISOString().split('T')[0];
-    return availableDates.includes(formattedDate) ? 'available-date' : '';
+    return availableDates.includes(formattedDate) ? 'bg-green-300' : '';
   };
 
   return (
-    <div className="calendar-container">
+    <div className="max-w-xs mx-auto">
       <Calendar
         onChange={setDate}
         value={date}
         tileClassName={tileClassName}
+        className="bg-white border border-gray-300 rounded shadow"
       />
     </div>
   );
