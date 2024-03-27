0f79a94be80d08606abf56fc16b75594ede4a480
Rachel Hamby <tnthreat33@gmail.com>
2024-03-27 11:11:33 -0400

    fix tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,9 @@
-// module.exports = {
-//   content: ['./src/**/*.html', './src/**/*.jsx'], // Scan HTML and JSX files in the src directory
-//   theme: {
-//     extend: {
-//       colors: {
-//         'cream-tan': '#f5deb3', // Define a custom color
-//       },
-//       fontFamily: {
-//         sans: ['Inter', 'sans-serif'], // Extend the default font family
-//       },
-//     },
-//   },
-//   plugins: [
-//     require('@tailwindcss/forms'), // Include the Tailwind CSS forms plugin
-//   ],
-// };
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
