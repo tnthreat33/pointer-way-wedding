cb595d1bef483f8d9e7df727f2e4913d5d242bed
Rachel Hamby <tnthreat33@gmail.com>
2024-04-02 13:46:40 -0400

    remove comments

--- src/App.js
+++ src/App.js
@@ -13,14 +13,14 @@ function App() {
   return (
     <div className="bg-slate-700 min-h-screen relative">
       <Navbar />
-      <div className="relative w-full overflow-hidden mb-8"> {/* Add margin-bottom to create space */}
+      <div className="relative w-full overflow-hidden mb-8"> 
         <img src={weddingUrl} alt="Main Wedding " className="w-full object-cover" style={{height: "650px"}} />
-        <div className="absolute top-1/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-slate-700 text-8xl font-cinzel"> {/* Position text in the middle */}
+        <div className="absolute top-1/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-slate-700 text-8xl font-cinzel"> 
           Pointers Way
         </div>
 
       </div>
-      <div id="about" className="py-10"> {/* Add padding only on top and bottom */}
+      <div id="about" className="py-10"> 
         <About/>
       </div>
       <div id="gallery">
